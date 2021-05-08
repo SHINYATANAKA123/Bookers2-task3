20a1321397cebe43eae4d9575aa387fe8f7b44e2
SHINYATANAKA123 <123asde.med@gmail.com>
2021-05-08 13:50:47 +0000

    [Add] Usersコントローラー追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,12 @@ class BooksController < ApplicationController
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
+
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root 'homes#top'
   resources :books
+  resources :users
 end
