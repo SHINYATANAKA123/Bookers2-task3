19655007f2554b2b6387511c457f206ec7c6972c
SHINYATANAKA123 <123asde.med@gmail.com>
2021-06-02 12:28:32 +0000

    [Add]いいね＆コメント

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :books
-  resources :users
   root to: 'homes#top'
   get 'home/about' =>'homes#about'
+  resources :users
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
 end
